77b68b9ce438cd9efe06c72e2792949abc8d8883
MuhammadM1998 <MuhammadM.1097@gmail.com>
2025-04-04 17:06:01 +0200

    refactor: don't sort data by default

--- app/components/content/chart/ContentChartPie.vue
+++ app/components/content/chart/ContentChartPie.vue
@@ -12,34 +12,11 @@
     options?: ApexOptions
   }>()
 
-  const sortedData = computed(() => {
-    if (!series?.length || !labels?.length) {
-      return { sortedSeries: [], sortedLabels: [] }
-    }
-
-    const combined = series.map((value, index) => ({
-      value,
-      label: labels[index],
-    }))
-
-    combined.sort((a, b) => Number(b.value) - Number(a.value))
-
-    return {
-      sortedSeries: combined.map((item) => item.value),
-      sortedLabels: combined
-        .map((item) => item.label)
-        .filter((label): label is string => label !== undefined),
-    }
-  })
-
   const mergedOptions = computed<ApexOptions>(() =>
-    defu(options, {
-      chart: { type: 'pie' as const },
-      labels: sortedData.value.sortedLabels,
-    }),
+    defu(options, { chart: { type: 'pie' as const }, labels }),
   )
 </script>
 
 <template>
-  <ContentChart :series="sortedData.sortedSeries" :options="mergedOptions" />
+  <ContentChart :series :options="mergedOptions" />
 </template>
