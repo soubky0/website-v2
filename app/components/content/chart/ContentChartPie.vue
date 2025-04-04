10841748f4d3af3db217bf6cc7ccc767d95d8192
MuhammadM1998 <MuhammadM.1097@gmail.com>
2025-04-04 17:02:12 +0200

    refactor: use props destructuring

--- app/components/content/chart/ContentChartPie.vue
+++ app/components/content/chart/ContentChartPie.vue
@@ -2,27 +2,24 @@
   import { defu } from 'defu'
   import type { ApexOptions } from 'apexcharts'
 
-  const props = withDefaults(
-    defineProps<{
-      series: ApexAxisChartSeries
-      labels: string[]
-      options?: ApexOptions
-    }>(),
-    {
-      series: () => [],
-      labels: () => [],
-      options: () => ({}),
-    },
-  )
+  const {
+    series = [],
+    labels = [],
+    options = {},
+  } = defineProps<{
+    series: ApexAxisChartSeries
+    labels: string[]
+    options?: ApexOptions
+  }>()
 
   const sortedData = computed(() => {
-    if (!props.series?.length || !props.labels?.length) {
+    if (!series?.length || !labels?.length) {
       return { sortedSeries: [], sortedLabels: [] }
     }
 
-    const combined = props.series.map((value, index) => ({
+    const combined = series.map((value, index) => ({
       value,
-      label: props.labels[index],
+      label: labels[index],
     }))
 
     combined.sort((a, b) => Number(b.value) - Number(a.value))
@@ -36,7 +33,7 @@
   })
 
   const mergedOptions = computed<ApexOptions>(() =>
-    defu(props.options, {
+    defu(options, {
       chart: { type: 'pie' as const },
       labels: sortedData.value.sortedLabels,
     }),
